578eede37682975511153cbe65f776b2ac6ae8a0
Raymond Kwan <rayk199492@gmail.com>
2018-09-10 22:04:55 +0000

    Added tweet rendering, and generating tweet form object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,74 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+function createTweetElement(db) {
+  const $tweet = $("<article>").addClass("single-tweet");
+  const header = `<header class="t-header"><img class="avatar" src="${db.user.avatars.small}"><strong class="username">${db.user.name}</strong><small class="user-tag">${db.user.handle}r</small></header>`
+  const body = `<p class="tweet-body">${db.content.text}</p>`
+  const footer = `<footer class="created-on">${new Date(db.created_at).toLocaleTimeString()}</footer>`
+
+  $tweet.append(header);
+  $tweet.append(body);
+  $tweet.append(footer);
+
+  return $tweet;
+}
+
+function renderTweet(arr) {
+  arr.forEach((tweet) => {
+    $('#tweet-container').append(createTweetElement(tweet));
+  });
+}
+
+$(document).ready(function () {
+  renderTweet(data);
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,14 +3,9 @@ console.log('hi');
 $(document).ready(function () {
   // --- our code goes here ---
   let count;
-  $('.new-tweet textarea').keyup(function(e) {
-    console.log('pressed', count);
+  $('.new-tweet textarea').on('input', function(e) {
     count = 140 - this.value.length;
     $(this).siblings('.counter').html(count);
-    if (count < 0) {
-      $(this).siblings('.counter').addClass('invalid');
-    } else {
-      $(this).siblings('.counter').removeClass('invalid');
-    }
+    $(this).siblings('.counter').toggleClass('invalid', count < 0);
   })
 });
