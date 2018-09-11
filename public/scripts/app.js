7953b724db82136266c1104c3319905406d3d3a4
Raymond Kwan <rayk199492@gmail.com>
2018-09-11 17:52:58 +0000

    Added calculations for days and weeks for post date

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,13 @@
  */
 
 function createTweetElement(db) {
-  const date = new Date(db.created_at).toLocaleTimeString();
+  const elaspe = Date.now() - db.created_at;
+  const days = Math.floor(elaspe / (8.64 * Math.pow(10, 7)));
+  const weeks = Math.floor(days / 7);
+  let ago = `${days} days ago`
+  if (weeks >= 1) {
+    ago = `${weeks} weeks ago`
+  }
   const $tweet = $("<article>").addClass("single-tweet");
   const header = `
     <header class="t-header">
@@ -14,7 +20,7 @@ function createTweetElement(db) {
       <small class="user-tag">${db.user.handle}r</small>
     </header>`
   const body = `<p class="tweet-body">${db.content.text}</p>`
-  const footer = `<footer class="created-on">${date}</footer>`
+  const footer = `<footer class="created-on">${ago}</footer>`
 
   $tweet.append(header);
   $tweet.append(body);
